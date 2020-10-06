603999c434a879d5a148e1488fb4d47c177775eb
Lip Cowan <pcowan90@gmail.com>
2020-10-06 08:41:31 -0700

    changed title in header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -43,7 +43,7 @@ class Header extends Component {
       <header>
         <h1>
           <Link to='/'>
-            Spaced repetition
+            Tentare
           </Link>
         </h1>
         {TokenService.hasAuthToken()
