6bf4eb74b5737c98c3a772af4d25bf36188740fe
Lip Cowan <pcowan90@gmail.com>
2020-10-06 09:11:42 -0700

    working on language service

---
+++ src/services/language-api-service.js
@@ -0,0 +1,14 @@
+import TokenService from './token-service';
+import config from '../config';
+
+const LanguageApiService = {
+    getLanguage() {
+// getAuthToken to confirm authentication and parseJwt for user.id ?        
+    },
+
+    getWords(languageId) {
+
+    }
+}
+
+export default LanguageApiService;
